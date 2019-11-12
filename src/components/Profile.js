de77cb6f02eb8657d7cb410831bdf8a4a02b124d
tigusanjh_sd2081@CEACCS.NET <tigusanjh_sd2081@CEACCS.NET>
2019-11-12 10:07:15 +0800

    Better UI

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
-import { Header, Form, Button } from 'semantic-ui-react'
+import {  Form, Input} from 'semantic-ui-react'
 import '../App.css'
-const options = [
-    { key: 'm', text: 'Male', value: 'male' },
-    { key: 'f', text: 'Female', value: 'female' },
-    { key: 'o', text: 'Other', value: 'other' },
-  ]
 
 export default class Profile extends Component {
     constructor(props){
@@ -17,71 +12,24 @@ export default class Profile extends Component {
     }
     state= {Input:''}
 
-    handleChange = (e, { value }) => this.setState({ value })
 
     render() {
-        return (
-            <div>
-                <div className="header">
-                    <Header>
-                        <img className="logo" alt="icon" src={require('../photos/logo.png')} />
-                    </Header>
-                    <Form>
-                        <Form.Group widths='equal'>
-                            <Form.Field
-                                control={this.state.Input}
-                                label='First name'
-                                placeholder='First name'
-                            />
-                            <Form.Field
-                                control={Input}
-                                label='Last name'
-                                placeholder='Last name'
-                            />
-                            <Form.Field
-                                control={Select}
-                                label='Gender'
-                                options={options}
-                                placeholder='Gender'
-                            />
-                        </Form.Group>
-                        <Form.Group inline>
-                            <label>Quantity</label>
-                            <Form.Field
-                                control={Radio}
-                                label='One'
-                                value='1'
-                                checked={value === '1'}
-                                onChange={this.handleChange}
-                            />
-                            <Form.Field
-                                control={Radio}
-                                label='Two'
-                                value='2'
-                                checked={value === '2'}
-                                onChange={this.handleChange}
-                            />
-                            <Form.Field
-                                control={Radio}
-                                label='Three'
-                                value='3'
-                                checked={value === '3'}
-                                onChange={this.handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Field
-                            control={TextArea}
-                            label='About'
-                            placeholder='Tell us more about you...'
-                        />
-                        <Form.Field
-                            control={Checkbox}
-                            label='I agree to the Terms and Conditions'
-                        />
-                        <Form.Field control={Button}>Submit</Form.Field>
-                    </Form>
-                </div>
-            </div>
-        );
+        // const { value } = this.state
+    return (
+      <Form>
+          <Form.Field label="What's My Dish?"/>
+        <Form.Group widths='equal'>
+
+          <Form.Input fluid placeholder='Dish Name' />
+          <Input type='file' fluid id='ImageUpload'/>
+          <Form.Input fluid placeholder='Description'/>
+        </Form.Group>
+        <Form.Group inline>
+
+        </Form.Group>
+        <Form.TextArea label='About' placeholder='Tell us more about you...' />
+        <Form.Button>Submit</Form.Button>
+      </Form>
+    )
     }
 }
